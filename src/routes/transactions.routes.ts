139c292d82122bfa526389416aa9fe5f15c6b1fb
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2020-05-11 23:58:15 -0300

    Criacao e listagem implementado.

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
 import CreateTransactionService from '../services/CreateTransactionService';
-
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 // [X] import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
@@ -10,13 +10,34 @@ import CreateTransactionService from '../services/CreateTransactionService';
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
 
-  const CreateTransaction = new CreateTransactionService();
+  /**
+
+Antes de criar uma nova categoria, sempre verifique se já existe uma categoria
+com o mesmo título. Caso ela exista, use o id já existente no banco de dados.
+
+ */
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,57 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    let findCategoryWithSameName = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!findCategoryWithSameName) {
+      findCategoryWithSameName = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(findCategoryWithSameName);
+    } else {
+      console.log({ message: 'This category already exist' });
+    }
+
+    /* else {
+      console.log({ message: 'This category already exist' });
+    } */
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: findCategoryWithSameName,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
